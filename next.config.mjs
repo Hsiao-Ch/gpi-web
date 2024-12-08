0320f0d15bd283fb7c9e29749945cebd13d1756e
hsiao <10856003@ntub.edu.tw>
2024-12-08 22:35:54 +0800

    修

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export", // <=== enables static exports
-  reactStrictMode: true,
-  basePath: "/gpi-web",
-  assetPrefix: "/gpi-web",
-  reactStrictMode: true,
-  distDir: 'dist',
-  images: {
-    unoptimized: true,
-  },
-};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: "export", // <=== enables static exports
+//   reactStrictMode: true,
+//   basePath: "/gpi-web",
+//   assetPrefix: "/gpi-web",
+//   reactStrictMode: true,
+//   distDir: 'dist',
+//   images: {
+//     unoptimized: true,
+//   },
+// };
 
-export default nextConfig;
+// export default nextConfig;
